f3effe18eeb73b4512fbdb2d2f18f4d2afe4955c
Marcos Santos <marcossamuel1710@outlook.com>
2020-01-08 10:32:38 -0300

    alteracao no caminho para salvar a imagem

--- controllers/uploadImageOcr.js
+++ controllers/uploadImageOcr.js
@@ -1,15 +1,13 @@
-const moment = require('moment')
-const path = require('path')
-
 module.exports = app => {
 
   // Método para realizar o upload da imagem do documento vindo do OCR
   const store = async (req, res) => {
     
     let img_documento  = req.file
+    let caminho = img_documento.path.substr(14)
 
     console.log({ message: 'Imagem do documento salva no storage' })
-    return res.status(200).json({ name: path.resolve(img_documento.path)})
+    return res.status(200).json({ name: 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + caminho})
   }
 
   return {
