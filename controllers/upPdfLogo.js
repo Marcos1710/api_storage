496f18087c27f62ba65ffd7b36e8631d0ec6fa5a
Marcos Santos <marcossamuel1710@outlook.com>
2020-01-15 15:34:08 -0300

    alterado rotas

--- controllers/upPdfLogo.js
+++ controllers/upPdfLogo.js
@@ -7,7 +7,7 @@ module.exports = app => {
     let caminho = pdf.path.substr(14)
 
     console.log({ message: 'Dados do hotel salvo no storage' })
-    return res.status(200).json({ name: 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + caminho })
+    return res.status(200).json({ name: 'https://st4-hfgtdg124cvt.economysoftware.com.br/' + caminho })
   }
 
   return {
--- controllers/uploadImageAss.js
+++ controllers/uploadImageAss.js
@@ -4,7 +4,7 @@ module.exports = app => {
   const store = async (req, res) => {
     
     let img_assinatura  = req.file
-    let caminho = 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + img_assinatura.path.substr(14)
+    let caminho = 'https://st4-hfgtdg124cvt.economysoftware.com.br/' + img_assinatura.path.substr(14)
     
     console.log({ message: 'Imagem da assinatura salva no storage' })
     return res.status(200).json({ name: caminho})
