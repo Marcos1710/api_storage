beeef8f08ca2c4b219d3f93cbe6c3e5c758dfc4a
Marcos Santos <marcossamuel1710@outlook.com>
2020-01-15 11:45:38 -0300

    alterado o retorno do metodo do pdf

--- controllers/upPdfLogo.js
+++ controllers/upPdfLogo.js
@@ -4,8 +4,10 @@ module.exports = app => {
   const store = async (req, res) => {
     
     let pdf  = req.file
+    let caminho = pdf.path.substr(14)
+
     console.log({ message: 'Dados do hotel salvo no storage' })
-    return res.status(200).json({ name: pdf.path })
+    return res.status(200).json({ name: 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + caminho })
   }
 
   return {
--- controllers/uploadImageOcr.js
+++ controllers/uploadImageOcr.js
@@ -7,7 +7,7 @@ module.exports = app => {
     let caminho = img_documento.path.substr(14)
 
     console.log({ message: 'Imagem do documento salva no storage' })
-    return res.status(200).json({ name: 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + caminho})
+    return res.status(200).json({ name: 'http://ec2-18-231-198-90.sa-east-1.compute.amazonaws.com/' + caminho })
   }
 
   return {
