66e04f62f6adcd4e47a8dec77ef80981d5e7e4f0
Marcos Santos <44325237+Marcos1710@users.noreply.github.com>
2020-01-22 15:53:00 -0300

    Update upPdfLogo.js

--- controllers/upPdfLogo.js
+++ controllers/upPdfLogo.js
@@ -7,7 +7,7 @@ module.exports = app => {
     let caminho = pdf.path.substr(14)
 
     console.log({ message: 'Dados do hotel salvo no storage' })
-    return res.status(200).json({ name: 'https://st4-hfgtdg124cvt.economysoftware.com.br/' + caminho })
+    return res.status(200).json({ name: 'link_url/' + caminho })
   }
 
   return {
